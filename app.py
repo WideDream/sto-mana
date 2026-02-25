e19f5631a5bc09161563a802f3a27a071361f7e7
WideDream <widedreamltd@gmail.com>
2026-02-25 20:05:42 +0200

    removed app.run

--- app.py
+++ app.py
@@ -108,6 +108,10 @@ def edit(id):
 
 
 import os
+from flask import Flask, render_template
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
